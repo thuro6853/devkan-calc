5bfa0febc8fb3d08cb858418edcbc5a4161ad997
irof <irof@hogedriven.net>
2013-07-06 23:25:51 +0900

    format

--- src/test/java/services/CalculatorTest.java
+++ src/test/java/services/CalculatorTest.java
@@ -1,15 +1,16 @@
 package services;
 
-import org.junit.*;
-import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
 
-  @Test
-  public void testAdd() {
-    Calculator sut = new Calculator();
-    String actual = sut.add(1, 1);
-    assertThat(actual, is("2"));
-  }
+    @Test
+    public void testAdd() {
+        Calculator sut = new Calculator();
+        String actual = sut.add(1, 1);
+        assertThat(actual, is("2"));
+    }
 }
